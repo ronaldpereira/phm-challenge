4da66ae94f9a199e0be5b41335b3aff9e7f9b5db
Ronald <ronald.drp11@gmail.com>
2019-09-24 15:03:09 -0300

    updated code

--- 2014/src/model_training_and_test.py
+++ 2014/src/model_training_and_test.py
@@ -5,15 +5,11 @@ from sklearn.model_selection import train_test_split
 
 RANDOM_SEED = 1212
 
-
 train = pd.read_csv("../data/train_features.csv")
-
 
 train.drop(["Time_failure", "Time_diff"], axis=1, inplace=True)
 
-
 train = pd.get_dummies(train, columns=["Asset", "Reason", "Part"])
-
 
 x_train, x_test, y_train, y_test = train_test_split(
     train.drop("Failure", axis=1),
@@ -23,20 +19,16 @@ x_train, x_test, y_train, y_test = train_test_split(
     random_state=RANDOM_SEED,
 )
 
-
 model = RandomForestClassifier(
     n_estimators=2000, n_jobs=-1, verbose=1, random_state=RANDOM_SEED
 )
 
-
 model.fit(x_train, y_train)
 
-joblib.dump(model, "random_forest_regressor_failure.joblib")
+joblib.dump(model, "random_forest_classifier_with_failure_time.joblib")
 
 probs = model.predict_proba(x_test)
 
-
 print(probs)
-
 
 sc = model.score(x_test, y_test)
